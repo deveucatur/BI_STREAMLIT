d3f9984ebcfde864c116ddead31d1c6d42275035
blackmen712 <pedrotivictor712@gmail.com>
2025-01-09 08:05:59 -0400

    login atualizações

--- Home.py
+++ Home.py
@@ -10,7 +10,43 @@ from conexao import conexaoBD
 from util import cabEscala, sideBar
 
 
+names = ['Victor Silva','Cleidimara Sander','Bruna Paio de Medeiros']
+usernames = ['pedrotivictor712@gmail.com','cleidi.sander@gmail.com' ,'performance.eucatur@gmail.com']
+hashed_passwords = ['admin','admin','admin']
 
+def convert_to_dict(names, usernames, passwords):
+    credentials = {"usernames": {}}
+    for name, username, password in zip(names, usernames, passwords):
+        user_credentials = {
+            "email":username,
+            "name": name,
+            "password": password
+        }
+        credentials["usernames"][username] = user_credentials
+    return credentials
+
+credentials = convert_to_dict(names, usernames, hashed_passwords)
+authenticator = stauth.Authenticate(credentials, "Teste", "abcde", cookie_expiry_days=30)
+
+col1, col2,col3 = st.columns([1,3,1])
+with col2:
+    name, authentication_status, username = authenticator.login(
+        location='main', fields={
+            'Form name':'Acessar BI sindicancia',
+            'Username':'Login', 
+            'Password':'Senha', 
+            'Login':'Entrar'
+            })
+
+
+if authentication_status == False:
+    with col2:
+        st.error('Email ou Senha Incorreto')
+elif authentication_status == None:
+    with col2:
+        st.warning('Insira seu Email e Senha')
+else:
+    authenticator.logout('Logout', 'sidebar') 
 
 def local_css(file_name):
     with open(file_name, encoding='utf-8') as f:
