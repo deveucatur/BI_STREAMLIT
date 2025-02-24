c472348ee41bda0e7cbffcd6a324157eb78b48ce
blackmen712 <pedrotivictor712@gmail.com>
2025-02-24 16:53:30 -0400

    e

--- pages/2 - BI Pagamentos.py
+++ pages/2 - BI Pagamentos.py
@@ -45,7 +45,7 @@ menu = cabEscala1(menu_menu)
 
 
 
-names = ['Victor Silva','Lucas','Lauro','André']
+names = ['Victor Silva','Lucas','Lauro','André','Luana']
 usernames = ['pedrotivictor712@gmail.com','controller.eucatur@gmail.com','lauro.processos.eucatur@gmail.com','andre.unep@gmail.com','contasapagar.eucatur4@gmail.com']
 hashed_passwords = ['admin','admin','admin','admin','admin123']
 
